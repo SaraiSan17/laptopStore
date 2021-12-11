bc90dada6e4c03a08605beb6d5ded08796c6ba67
Flor-Sarai-Santiago <75344988+SaraiSan17@users.noreply.github.com>
2021-12-10 18:06:23 -0600

    Add files via upload

---
+++ api/rutas/rutas.js
@@ -0,0 +1,78 @@
+const express=require('express')
+const router=express.Router();
+
+var productos=[{ 
+	"Nombre":"HP PAVILION",
+	"Descripcion": "8 GB RAM, COLOR PLATEADO, 256 GB DISCO SSD",
+	"Imagenid":"..img/hp1.jpg",
+	"Precio":"5000"
+},
+
+{ 
+	"Nombre":"ACER",
+	"Descripcion": "16 GB RAM, COLOR NEGRO, 1 TB DD",
+	"Imagenid":"..img/acer.jpg",
+	"Precio":"3000"
+},
+{ 
+	"Nombre":"LENOVO",
+	"Descripcion": "4 GB RAM, COLOR PLATEADO, 1 TB DD",
+	"Imagenid":"..img/lenovo.jpg",
+	"Precio":"3000"
+},
+{ 
+	"Nombre":"APPLE",
+	"Descripcion": "8 GB RAM, COLOR GOLD, 128 GB DD",
+	"Imagenid":"..img/apple.jpg",
+	"Precio":"5900"
+},
+{ 
+	"Nombre":"HUAWEI",
+	"Descripcion": "8 GB RAM, COLOR NEGRO, 256 GB DD",
+	"Imagenid":"..img/huawei.jpg",
+	"Precio":"5769"
+},
+{ 
+	"Nombre":"SAMSUNG",
+	"Descripcion": "4 GB RAM, COLOR BLANCA, 64 GB DD",
+	"Imagenid":"..img/samsung.jpg",
+	"Precio":"2599"
+},
+{ 
+	"Nombre":"LG",
+	"Descripcion": "8 GB RAM, COLOR PLATEADO, 256 GB DD",
+	"Imagenid":"..img/lg.jpg",
+	"Precio":"4299"
+},
+{ 
+	"Nombre":"ADVANCE",
+	"Descripcion": "3 GB RAM, COLOR NEGRO, 64 GB DD",
+	"Imagenid":"..img/advance.jpg",
+	"Precio":"869"
+},
+{ 
+	"Nombre":"DELL",
+	"Descripcion": "8 GB RAM, COLOR NEGRO, 1 TB DD",
+	"Imagenid":"..img/dell.jpg",
+	"Precio":"5397"
+}
+
+]
+
+var pedidos = [];
+
+router.get('/productos/', function(req, res) {
+    res.json(productos);
+});
+router.get('/pedidos/', function(req, res) {
+    res.json(pedidos);
+})
+router.post('/pedidos/', function(req, res) {
+    pedidos.push(req.body);
+    res.json({"status":"insertado"});
+})
+
+
+
+
+module.exports=router;
