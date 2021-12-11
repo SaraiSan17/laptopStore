507708d17a0f42d5031d9be7d65902b57c02ae45
Flor-Sarai-Santiago <75344988+SaraiSan17@users.noreply.github.com>
2021-12-10 18:16:55 -0600

    Update rutas.js

--- api/rutas/rutas.js
+++ api/rutas/rutas.js
@@ -4,56 +4,56 @@ const router=express.Router();
 var productos=[{ 
 	"Nombre":"HP PAVILION",
 	"Descripcion": "8 GB RAM, COLOR PLATEADO, 256 GB DISCO SSD",
-	"Imagenid":"..img/hp1.jpg",
+	"Imagenid":"img/hp1.jpg",
 	"Precio":"5000"
 },
 
 { 
 	"Nombre":"ACER",
 	"Descripcion": "16 GB RAM, COLOR NEGRO, 1 TB DD",
-	"Imagenid":"..img/acer.jpg",
+	"Imagenid":"img/acer.jpg",
 	"Precio":"3000"
 },
 { 
 	"Nombre":"LENOVO",
 	"Descripcion": "4 GB RAM, COLOR PLATEADO, 1 TB DD",
-	"Imagenid":"..img/lenovo.jpg",
+	"Imagenid":"img/lenovo.jpg",
 	"Precio":"3000"
 },
 { 
 	"Nombre":"APPLE",
 	"Descripcion": "8 GB RAM, COLOR GOLD, 128 GB DD",
-	"Imagenid":"..img/apple.jpg",
+	"Imagenid":"img/apple.jpg",
 	"Precio":"5900"
 },
 { 
 	"Nombre":"HUAWEI",
 	"Descripcion": "8 GB RAM, COLOR NEGRO, 256 GB DD",
-	"Imagenid":"..img/huawei.jpg",
+	"Imagenid":"img/huawei.jpg",
 	"Precio":"5769"
 },
 { 
 	"Nombre":"SAMSUNG",
 	"Descripcion": "4 GB RAM, COLOR BLANCA, 64 GB DD",
-	"Imagenid":"..img/samsung.jpg",
+	"Imagenid":"img/samsung.jpg",
 	"Precio":"2599"
 },
 { 
 	"Nombre":"LG",
 	"Descripcion": "8 GB RAM, COLOR PLATEADO, 256 GB DD",
-	"Imagenid":"..img/lg.jpg",
+	"Imagenid":"img/lg.jpg",
 	"Precio":"4299"
 },
 { 
 	"Nombre":"ADVANCE",
 	"Descripcion": "3 GB RAM, COLOR NEGRO, 64 GB DD",
-	"Imagenid":"..img/advance.jpg",
+	"Imagenid":"img/advance.jpg",
 	"Precio":"869"
 },
 { 
 	"Nombre":"DELL",
 	"Descripcion": "8 GB RAM, COLOR NEGRO, 1 TB DD",
-	"Imagenid":"..img/dell.jpg",
+	"Imagenid":"img/dell.jpg",
 	"Precio":"5397"
 }
 
